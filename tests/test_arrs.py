776740000ef9ac14d79649240bdaf527941db0e4
Ilfat Windows <galinurov.ilfat@gmail.com>
2023-06-16 22:47:44 +0300

    'Добавление фикстуры для теста slice'

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,9 +6,9 @@ def test_get():
     assert arrs.get([], -5, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(array_fixture):
+    assert arrs.my_slice(array_fixture, 1, 3) == array_fixture[1:3]
+    assert arrs.my_slice(array_fixture, 1) == array_fixture[1:]
     assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3], -10) == [1, 2, 3]
-    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+    assert arrs.my_slice(array_fixture, -10) == array_fixture
+    assert arrs.my_slice(array_fixture, -2) == array_fixture[-2:]
